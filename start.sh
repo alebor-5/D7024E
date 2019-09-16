ae8bf3c1625dacb46d5b9286645409e11c0d1a24
dejohansson <davjom-5@student.ltu.se>
2019-09-16 22:42:21 +0200

    Added script for building the docker image.

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ go build -o ./bin/kademlia ./src/kademlia/
 echo "Restarting Stack"
 for i in $(seq 0 $n); do 
     (docker stack deploy -c docker-compose.yml kademlia) && break
-    if [ $i < $n ]; then
+    if [ $i != $n ]; then
         echo "Trying again in $interval second(s)...";
         sleep $interval;
     fi;
