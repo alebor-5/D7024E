c04412968d587016b9430d514f6cfbadf7d8c0a4
dejohansson <davjom-5@student.ltu.se>
2019-09-18 15:16:42 +0200

    put and get can now parse args containing spaces

--- src/kademlia/cli.go
+++ src/kademlia/cli.go
@@ -4,43 +4,58 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
 	"strings"
 )
 
 func cli() {
 	scanner := bufio.NewScanner(os.Stdin)
+	cmdExp := regexp.MustCompile(`^\s*\S+($|\s)`)
+	spExp := regexp.MustCompile(`^\s*$`)
+	strExp := regexp.MustCompile(`^".*"$`)
 	for {
 		fmt.Print(">")
 		scanner.Scan()
-		args := strings.Fields(scanner.Text())
+		input := scanner.Text()
 
-		if len(args) > 0 {
-			switch strings.ToLower(args[0]) {
+		if cmdExp.MatchString(input) {
+			rawcmd := cmdExp.FindString(input)
+			args := strings.TrimSpace(input[len(rawcmd):])
+			cmd := strings.TrimSpace(rawcmd)
+
+			switch strings.ToLower(cmd) {
 			case "put":
-				if len(args) != 2 {
-					fmt.Println("put takes exactly 1 argument! [put <file_content>]")
-				} else {
+				if strExp.MatchString(args) {
 					fmt.Println("Store isn't implemented :(")
+					fmt.Println("IP: " + args[1:len(args)-1])
 					//TODO: Run Store, if successful print the object hash.
+				} else {
+					fmt.Println("put takes exactly 1 argument! e.g. [put \"111.111.111:0000\"]")
 				}
 			case "get":
-				if len(args) != 2 {
-					fmt.Println("get takes exactly 1 argument! [get <hash>]")
-				} else {
+				if strExp.MatchString(args) {
 					fmt.Println("LookupData isn't implemented :(")
+					fmt.Println("Key: " + args[1:len(args)-1])
 					//TODO: Run LookupData, if successful print the object content.
+				} else {
+					fmt.Println("get takes exactly 1 argument! e.g. [get \"48656c6c6f2066726f6d20414\"]")
 				}
 			case "exit":
-				fmt.Print("This will terminate the node. Continue? [y/n]: ")
-				scanner.Scan()
-				if strings.ToLower(scanner.Text()) == "y" {
-					fmt.Println("Exiting node...")
-					os.Exit(0)
+				if spExp.MatchString(args) {
+					fmt.Print("This will terminate the node. Continue? [y/n]: ")
+					scanner.Scan()
+					if strings.ToLower(scanner.Text()) == "y" {
+						fmt.Println("Exiting node...")
+						os.Exit(0)
+					}
+					fmt.Println("Termination aborted")
+				} else {
+					fmt.Println("exit doesn't take any arguments!")
 				}
-				fmt.Println("Termination aborted")
 			default:
-				fmt.Println("Unknown command: " + args[0])
+				fmt.Println("Unknown command: " + cmd)
 			}
+
 		}
 	}
 }
