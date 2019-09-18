db8e015bc238a2159cea66131508094475e3ba90
dejohansson <davjom-5@student.ltu.se>
2019-09-18 17:26:18 +0200

    CLI test setup.

--- src/kademlia/cli.go
+++ src/kademlia/cli.go
@@ -3,48 +3,50 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
 	"regexp"
 	"strings"
 )
 
-func cli() {
-	scanner := bufio.NewScanner(os.Stdin)
+func cli(stdin io.Reader) {
+	reader := bufio.NewReader(stdin)
 	cmdExp := regexp.MustCompile(`^\s*\S+($|\s)`)
 	spExp := regexp.MustCompile(`^\s*$`)
 	strExp := regexp.MustCompile(`^".*"$`)
 	for {
 		fmt.Print(">")
-		scanner.Scan()
-		input := scanner.Text()
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSuffix(input, "\n")
 
 		if cmdExp.MatchString(input) {
-			rawcmd := cmdExp.FindString(input)
-			args := strings.TrimSpace(input[len(rawcmd):])
-			cmd := strings.TrimSpace(rawcmd)
+			cmd := cmdExp.FindString(input)
+			args := strings.TrimSpace(input[len(cmd):])
+			cmd = strings.TrimSpace(cmd)
 
 			switch strings.ToLower(cmd) {
 			case "put":
 				if strExp.MatchString(args) {
 					fmt.Println("Store isn't implemented :(")
-					fmt.Println("IP: " + args[1:len(args)-1])
+					fmt.Println("Value: " + args[1:len(args)-1])
 					//TODO: Run Store, if successful print the object hash.
 				} else {
-					fmt.Println("put takes exactly 1 argument! e.g. [put \"111.111.111:0000\"]")
+					fmt.Println("put takes exactly 1 argument! e.g. [put \"48656c6c6f2066726f6d20414\"]")
 				}
 			case "get":
 				if strExp.MatchString(args) {
 					fmt.Println("LookupData isn't implemented :(")
-					fmt.Println("Key: " + args[1:len(args)-1])
+					fmt.Println("IP: " + args[1:len(args)-1])
 					//TODO: Run LookupData, if successful print the object content.
 				} else {
-					fmt.Println("get takes exactly 1 argument! e.g. [get \"48656c6c6f2066726f6d20414\"]")
+					fmt.Println("get takes exactly 1 argument! e.g. [get \"111.111.111:0000\"]")
 				}
 			case "exit":
 				if spExp.MatchString(args) {
 					fmt.Print("This will terminate the node. Continue? [y/n]: ")
-					scanner.Scan()
-					if strings.ToLower(scanner.Text()) == "y" {
+					answer, _ := reader.ReadString('\n')
+					answer = strings.TrimSuffix(answer, "\n")
+					if strings.ToLower(answer) == "y" {
 						fmt.Println("Exiting node...")
 						os.Exit(0)
 					}
@@ -55,7 +57,6 @@ func cli() {
 			default:
 				fmt.Println("Unknown command: " + cmd)
 			}
-
 		}
 	}
 }
--- src/kademlia/main.go
+++ src/kademlia/main.go
@@ -1,8 +1,10 @@
 package main
 
+import "os"
+
 func main() {
 	node := NewKademliaNode()
 	node.PrintIP()
 
-	cli()
+	cli(os.Stdin)
 }
