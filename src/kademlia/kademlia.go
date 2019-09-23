6a1c1bdb85a11f84dabbe479e20676589a0734ab
dejohansson <davjom-5@student.ltu.se>
2019-09-23 11:27:44 +0200

    Needs iterative FIND_NODE to continue.

--- src/kademlia/kademlia.go
+++ src/kademlia/kademlia.go
@@ -5,16 +5,27 @@ import (
 )
 
 type Kademlia struct {
-	id KademliaID
-	ip string
+	id           KademliaID
+	ip           string
 	routingTable RoutingTable
+}
+
+func JoinNetwork(bootstrapID *KademliaID, bootstrapIP string) Network {
+	node := InitKademliaNode()
+	network := Network{&node}
+	node.routingTable.AddContact(NewContact(bootstrapID, bootstrapIP))
+	go network.Listen()
+	//TODO: Run iterative FIND_NODE on self
+	network.SendPingMessage(NewContact(bootstrapID, bootstrapIP))
+	//TODO: Refresh all buckets further away than the closest neighbor
+	return network
 }
 
 func InitKademliaNode() Kademlia {
 	id := NewRandomKademliaID()
 	ip := GetIP()
-	rt := NewRoutingTable(NewContact(id,ip))
-	return  Kademlia{*id, GetIP(), *rt }
+	rt := NewRoutingTable(NewContact(id, ip))
+	return Kademlia{*id, GetIP(), *rt}
 }
 
 func (kademlia *Kademlia) LookupContact(target *Contact) {
--- src/kademlia/main.go
+++ src/kademlia/main.go
@@ -9,23 +9,14 @@ func main() {
 	isBootstrapPtr := flag.Bool("bootstrap", false, "If false the node will try to join the hardcoded bootstrap node at startup")
 	bootstrapIP := "10.0.0.2"
 	bootstrapID := NewKademliaID("630f496249240d231d61365161424d442c040761")
-	bootstrapNode := Kademlia{*bootstrapID, bootstrapIP, *NewRoutingTable(NewContact(bootstrapID, bootstrapIP))}
 	var network Network
-	var node Kademlia
 
 	flag.Parse()
 	if *isBootstrapPtr {
-		//TODO: Bootstrap setup
-		node = bootstrapNode
-		network = Network{&node}
+		network = Network{&Kademlia{*bootstrapID, bootstrapIP, *NewRoutingTable(NewContact(bootstrapID, bootstrapIP))}}
 		go network.Listen()
 	} else {
-		//TODO: Genral setup
-		//TODO: Try to join bootstrap node at: 10.0.0.2
-		node = InitKademliaNode()
-		network = Network{&node}
-		go network.Listen()
-		network.SendPingMessage(NewContact(bootstrapID, bootstrapIP))
+		network = JoinNetwork(bootstrapID, bootstrapIP)
 	}
 	network.cliLoop(os.Stdin)
 }
