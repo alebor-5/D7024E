91ae8606f9db80c324b71a938f39ac832377c13a
dejohansson <davjom-5@student.ltu.se>
2019-09-23 09:48:34 +0200

    Kademlianodes now pings the bootstrapnode at start

--- src/kademlia/main.go
+++ src/kademlia/main.go
@@ -7,20 +7,25 @@ import (
 
 func main() {
 	isBootstrapPtr := flag.Bool("bootstrap", false, "If false the node will try to join the hardcoded bootstrap node at startup")
-	// bootstrapIP := "10.0.0.2"
-	// bootstrapID := NewKademliaID("630f496249240d231d61365161424d442c040761")
-	// bootstrapNode := NewContact(bootstrapID, bootstrapIP)
-	node := InitKademliaNode()
-	node.PrintIP()
-	network := Network{&node}
-	go network.Listen()
+	bootstrapIP := "10.0.0.2"
+	bootstrapID := NewKademliaID("630f496249240d231d61365161424d442c040761")
+	bootstrapNode := Kademlia{*bootstrapID, bootstrapIP, *NewRoutingTable(NewContact(bootstrapID, bootstrapIP))}
+	var network Network
+	var node Kademlia
+
 	flag.Parse()
 	if *isBootstrapPtr {
 		//TODO: Bootstrap setup
+		node = bootstrapNode
+		network = Network{&node}
+		go network.Listen()
 	} else {
 		//TODO: Genral setup
 		//TODO: Try to join bootstrap node at: 10.0.0.2
-		network.SendPingMessage("10.0.0.2")
+		node = InitKademliaNode()
+		network = Network{&node}
+		go network.Listen()
+		network.SendPingMessage(NewContact(bootstrapID, bootstrapIP))
 	}
 	network.cliLoop(os.Stdin)
 }
