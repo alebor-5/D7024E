7ff7e6f644661bf1e2c0e7afc6ff05205eb9c66b
DEJohansson <33880373+DEJohansson@users.noreply.github.com>
2019-09-24 00:15:26 +0200

    Cleaned up some of the config files.

--- start.sh
+++ start.sh
@@ -6,18 +6,3 @@ echo "Deploying bootstrap node:"
 docker-compose up -d bootstrap-node
 echo "Deploying kademlia nodes:"
 docker-compose up -d --scale kademlia-node=10 kademlia-node
-
-# interval=1 #Number of seconds to wait
-# n=10 #Number of tries
-
-# docker stack rm kademlia
-# echo "Building app"
-# go build -o ./bin/kademlia ./src/kademlia/
-# echo "Restarting Stack"
-# for i in $(seq 0 $n); do 
-#     (docker stack deploy -c docker-compose.yml kademlia) && break
-#     if [ $i != $n ]; then
-#         echo "Trying again in $interval second(s)...";
-#         sleep $interval;
-#     fi;
-# done
