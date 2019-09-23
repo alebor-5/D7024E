e2f23a7b571aa4cd7b8f022cfad5632f783a449e
DEJohansson <33880373+DEJohansson@users.noreply.github.com>
2019-09-24 00:32:17 +0200

    Made the start script more intuetive.

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
+nodes=10 # number of total nodes (including the bootstrap node) that should be deployed
 echo "Removing any old services:"
-docker-compose rm -sf bootstrap-node kademlia-node
+docker-compose rm -sf bootstrap-node kademlia-node # -s, stops any running kademlia nodes. -f force removal of the containers.
 echo "Building app:"
 go build -o ./bin/kademlia ./src/kademlia/
 echo "Deploying bootstrap node:"
-docker-compose up -d bootstrap-node
+docker-compose up -d bootstrap-node # -d, starts in detached mode
 echo "Deploying kademlia nodes:"
-docker-compose up -d --scale kademlia-node=10 kademlia-node
+docker-compose up -d --scale kademlia-node=($nodes-1) kademlia-node # -d, starts in detached mode
