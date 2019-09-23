44290074b17ae32c613c442d6771b26ce444acb7
dejohansson <davjom-5@student.ltu.se>
2019-09-23 09:20:11 +0200

    Pre-merge commit

--- src/kademlia/main.go
+++ src/kademlia/main.go
@@ -7,16 +7,20 @@ import (
 
 func main() {
 	isBootstrapPtr := flag.Bool("bootstrap", false, "If false the node will try to join the hardcoded bootstrap node at startup")
+	// bootstrapIP := "10.0.0.2"
+	// bootstrapID := NewKademliaID("630f496249240d231d61365161424d442c040761")
+	// bootstrapNode := NewContact(bootstrapID, bootstrapIP)
+	node := InitKademliaNode()
+	node.PrintIP()
+	network := Network{&node}
+	go network.Listen()
 	flag.Parse()
 	if *isBootstrapPtr {
 		//TODO: Bootstrap setup
 	} else {
 		//TODO: Genral setup
 		//TODO: Try to join bootstrap node at: 10.0.0.2
+		network.SendPingMessage("10.0.0.2")
 	}
-	node := InitKademliaNode()
-	node.PrintIP()
-	network := Network{&node}
-	go network.Listen()
 	network.cliLoop(os.Stdin)
 }
