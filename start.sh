952298f230e6394e16cf25ebed81d0bfb16d2f5e
dejohansson <davjom-5@student.ltu.se>
2019-09-27 12:25:20 +0200

    Fixed bugg when starting too many nodes.

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-nodes=10 # number of total nodes (including the bootstrap node) that should be deployed
+nodes=${1:-50} # number of total nodes (including the bootstrap node) that should be deployed
 echo "Removing any old services:"
 docker-compose rm -sf bootstrap-node kademlia-node # -s, stops any running kademlia nodes. -f force removal of the containers.
 echo "Building app:"
@@ -6,4 +6,9 @@ go build -o ./bin/kademlia ./src/kademlia/
 echo "Deploying bootstrap node:"
 docker-compose up -d bootstrap-node # -d, starts in detached mode
 echo "Deploying kademlia nodes:"
-docker-compose up -d --scale kademlia-node=($nodes-1) kademlia-node # -d, starts in detached mode
+i=$([ $(((nodes-1)%10)) == 0 ] && (echo 10) || (echo $(((nodes-1)%10))))
+while [ $i -lt $(( nodes )) ]
+do 
+    docker-compose up -d --scale kademlia-node=$i kademlia-node # -d, starts in detached mode
+    i=$(( i+10 ))
+done
