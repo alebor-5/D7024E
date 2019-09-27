614a599c69493c8034c14a6000efdda49d071322
dejohansson <davjom-5@student.ltu.se>
2019-09-27 12:57:59 +0200

    Added routingtable mutex

--- src/kademlia/rpc.go
+++ src/kademlia/rpc.go
@@ -6,26 +6,29 @@ import (
 )
 
 type RPC struct {
-	
 }
 
 func (network *Network) HandleRequest(packet Packet, conn *net.UDPConn, addr *net.UDPAddr) {
 	contact := NewContact(NewKademliaID(packet.Header.NodeID), packet.Header.IP)
-	switch packet.Header.RPC{
-		case "PING":
-			network.kademlia.routingTable.AddContact(contact)
-			network.SendResponse("PONG", "", conn, addr)
-		default:
-			fmt.Println("UNKNOWN REQUEST RPC: " + packet.Header.RPC)
+	switch packet.Header.RPC {
+	case "PING":
+		network.kademlia.routingTable.mux.Lock()
+		network.kademlia.routingTable.AddContact(contact)
+		network.kademlia.routingTable.mux.Unlock()
+		network.SendResponse("PONG", "", conn, addr)
+	default:
+		fmt.Println("UNKNOWN REQUEST RPC: " + packet.Header.RPC)
 	}
 }
 
-func (network *Network) HandleResponse(packet Packet){
+func (network *Network) HandleResponse(packet Packet) {
 	contact := NewContact(NewKademliaID(packet.Header.NodeID), packet.Header.IP)
-	switch packet.Header.RPC{
-		case "PONG":
-			network.kademlia.routingTable.AddContact(contact)
-		default:
-			fmt.Println("UNKNOWN RESPONSE RPC: " + packet.Header.RPC)
+	switch packet.Header.RPC {
+	case "PONG":
+		network.kademlia.routingTable.mux.Lock()
+		network.kademlia.routingTable.AddContact(contact)
+		network.kademlia.routingTable.mux.Unlock()
+	default:
+		fmt.Println("UNKNOWN RESPONSE RPC: " + packet.Header.RPC)
 	}
 }
